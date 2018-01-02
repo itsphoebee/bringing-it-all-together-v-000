28709768f40189f949373f29e50c2b40866dd79f
Phoebe Lau <phoebelau7@gmail.com>
2018-01-02 18:51:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,8 +32,8 @@ class Dog
         VALUES (?, ?)
       SQL
 
-      DB[:conn].execute(sql, self.name, self.breed)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      DB[:conn].execute(sql, self.name, self.breed).flatten
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0]
     end
     self
   end

ee712cff87e08a3c9d3b7bb7fbd6ecfc19736702
Phoebe Lau <phoebelau7@gmail.com>
2018-01-02 18:38:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,14 +47,7 @@ class Dog
   end
 
   def self.find_by_id(id)
-    sql = <<-SQL
-    SELECT *
-    FROM dogs
-    WHERE id = ?
-    LIMIT 1
-    SQL
-
-    DB[:conn].execute(sql,id).map do |row|
+    DB[:conn].execute("SELECT * FROM dogs WHERE id = ? LIMIT 1",id).map do |row|
       self.new_from_db(row)
     end.first
   end

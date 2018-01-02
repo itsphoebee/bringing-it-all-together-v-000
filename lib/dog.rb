5301a6abd5063885f8197dd52415a08d4a093995
Phoebe Lau <phoebelau7@gmail.com>
2018-01-02 18:34:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -42,7 +42,8 @@ class Dog
     new_dog.save
   end
 
-  def self.new_from_db
+  def self.new_from_db(row)
+    dog = Dog.new(id:row[0], name:row[1], breed:row[2])
   end
 
   def self.find_by_id(id)
